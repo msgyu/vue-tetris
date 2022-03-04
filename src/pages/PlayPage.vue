eedac8a249df41392bc738929ff2d787f686ffa0
柳生真輝 <nousidayo@gmail.com>
2022-03-04 21:04:36 +0900

    block別にclassを出力

--- src/pages/PlayPage.vue
+++ src/pages/PlayPage.vue
@@ -9,11 +9,36 @@ for(let i=0; i<row; i++) {
   field[i] = fieldColumn;
 }
 
-// 画面最上部の左端の縦のI-テトリミノを配置する
+// 画面最上部の左端の縦のl-テトリミノを配置する
 field[0][0] = 1;
 field[1][0] = 1;
 field[2][0] = 1;
 field[3][0] = 1;
+
+const classBlockColor = (x: number, y: number): string => {
+  const type = field[y][x];
+  if (type > 0) {
+    switch (type) {
+      case 1:
+        return "block-i";
+      case 2:
+        return "block-o";
+      case 3:
+        return "block-s";
+      case 4:
+        return "block-z";
+      case 5:
+        return "block-j";
+      case 6:
+        return "block-l";
+      case 7:
+        return "block-t";
+      default:
+        return "";
+    }
+  }
+  return "";
+}
 
 </script>
 
@@ -29,31 +54,55 @@ field[3][0] = 1;
           class="block"
           v-for="(col, x) in row"
           :key="() => `${x}${y}`"
-        >
-          {{ col }}
-        </td>
+          :class="classBlockColor(x, y)"
+        />
       </tr>
     </table>
 
   </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
 .container {
    display: flex;
    justify-content: center;
    align-items: stretch;
- }
+}
  
  .field {
    border: ridge 0.4em #2c3e50;
    border-top: none;
- }
+}
  
- .block {
-   width: 1em;
-   height: 1em;
-   border: 0.1px solid #95a5a6;
- }
+.block {
+  width: 1em;
+  height: 1em;
+  border: 0.1px solid #95a5a6;
+    /*
+    各テトリミノに対応した色を扱うクラス定義
+    .block-i, .block-o のようにクラスが定義される
+    */
+    &-i {
+      background: #3498db;
+    }
+    &-o {
+      background: #f1c40f;
+    }
+    &-t {
+      background: #9b59b6;
+    }
+    &-j {
+      background: #1e3799;
+    }
+    &-l {
+      background: #e67e22;
+    }
+    &-s {
+      background: #2ecc71;
+    }
+    &-z {
+      background: #e74c3c;
+    }
+}
   
 </style>
